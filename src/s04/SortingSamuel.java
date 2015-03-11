bb33df063d46ff385841a7877569080175ac0c31
liTTleSiG <myrae4@gmail.com>
2015-03-11 22:56:35 +0100

    Algo/s04// shellSort :shell:

--- src/s04/SortingSamuel.java
+++ src/s04/SortingSamuel.java
@@ -1,51 +1,71 @@
 package s04;
+
 public class SortingSamuel {
-  public static void main(String [] args) {
-    int [] t = {4, 3, 2, 6, 8, 7};
-    int [] u = {2, 3, 4, 6, 7, 8};
-    selectionSort(t);
-    for (int i=0; i<t.length; i++) 
-      if(t[i] != u[i]) {
-        System.out.println("Something is wrong..."); return;
+  public static void main(String[] args) {
+    int[] t = { 4, 3, 2, 6, 8, 7 };
+    int[] u = { 2, 3, 4, 6, 7, 8 };
+    shellSort(t);
+    for (int i = 0; i < t.length; i++)
+      if (t[i] != u[i]) {
+        System.out.println("Something is wrong...");
+        return;
       }
-    int [] v = {5};
-    selectionSort(v);
-    int [] w = {};
-    selectionSort(w);
+    int[] v = { 5 };
+    shellSort(v);
+    int[] w = {};
+    shellSort(w);
     System.out.println("\nMini-test passed successfully...");
   }
-  //------------------------------------------------------------
-  public static void selectionSort(int [] a){
-    int i,j,v;
-    for (i=0;i<a.length;i++){
-      for(j=i,v=a[j];j<a.length;j++)
-      { 
-        v=a[j];
-        if(a[i]>a[j])
-        {
-          a[j]=a[i];
-          a[i]=v;
+
+  // ------------------------------------------------------------
+  public static void selectionSort(int[] a) {
+    int i, j, v, min;
+    for (i = 0; i < a.length; i++) {
+      for (j = i, min = a[j], v = j; j < a.length; j++) {
+        if (min > a[j]) {
+          min = a[j];
+          v = j;
         }
       }
+      a[v] = a[i];
+      a[i] = min;
     }
   }
-  //------------------------------------------------------------
-  public static void shellSort(int [] a){
-    int i,j,k=4;
+
+  // ------------------------------------------------------------
+  public static void shellSort(int[] a) {
+    int i, j, l, v, min;
+    int k = 4;
+    while (k >= 1) {
+      for (l = 0; l < k; l++) {
+        for (i = l; i < a.length; i += k) {
+          for (j = i, min = a[j], v = j; j < a.length; j += k) {
+            if (min > a[j]) {
+              min = a[j];
+              v = j;
+            }
+          }
+          a[v] = a[i];
+          a[i] = min;
+        }
+      }
+      k = k / 2;
+    }
   }
-  //------------------------------------------------------------
-  public static void insertionSort(int [] a){
+
+  // ------------------------------------------------------------
+  public static void insertionSort(int[] a) {
     int i, j, v;
 
-    for (i=1; i<a.length; i++) {
+    for (i = 1; i < a.length; i++) {
       v = a[i];          // v is the element to insert
       j = i;
-      while (j>0 && a[j-1] > v) {
-        a[j] = a[j-1];   // move to the right
+      while (j > 0 && a[j - 1] > v) {
+        a[j] = a[j - 1];   // move to the right
         j--;
       }
       a[j] = v;          // insert the element
     }
   }
-  //------------------------------------------------------------
+  // ------------------------------------------------------------
 }
