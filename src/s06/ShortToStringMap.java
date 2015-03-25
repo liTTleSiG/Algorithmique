4270ad1d4356054ccb6294f0a8df2e37ed1ce66b
brunoproduit <bruno.produit@gmail.com>
2015-03-25 15:17:57 +0100

    Algo/s06/ et encore

--- src/s06/ShortToStringMap.java
+++ src/s06/ShortToStringMap.java
@@ -1,71 +1,90 @@
 package s06;
+
 public class ShortToStringMap {
-  private int size;
-  short[] id=new short[50];
-  String[] content=new String[50];
+  private int                 size;
+  short[]                     id      = new short[50];
+  String[]                    content = new String[50];
   private ShortToStringMapItr iterateur;
+
   // TODO - A COMPLETER...
-  //------------------------------
-  //  Private methods
-  //------------------------------
-  private boolean locateKey(int i)
-  {
-    
+  // ------------------------------
+  // Private methods
+  // ------------------------------
+  private boolean locateKey(int i) {
+
     return true;
   }
+
   // Could be useful, for instance :
   // - one method to detect and handle the "array is full" situation
-  // - one method to locate a key in the array 
-  //   (to be called from containsKey(), put(), and remove())
+  // - one method to locate a key in the array
+  // (to be called from containsKey(), put(), and remove())
 
-  //------------------------------
-  //  Public methods
-  //------------------------------
+  // ------------------------------
+  // Public methods
+  // ------------------------------
   // ------------------------------------------------------------
-  public ShortToStringMap () {
-    iterateur= new ShortToStringMapItr(this);
+  public ShortToStringMap() {
+    iterateur = new ShortToStringMapItr(this);
   }
+
   // ------------------------------------------------------------
   // adds an entry in the map, or updates the image
-  public void    put     (short key, String img) {
+  public void put(short key, String img) {
     // TODO - A COMPLETER...
-  } 
+  }
+
   // ------------------------------------------------------------
   // returns null if !containsKey(key)
-  public String  get     (short key) {
+  public String get(short key) {
     if (!containsKey(key))
-    return null;
+      return null;
     else
-      return " ";
-      
+      return " "; // a completer
+
   }
+
   // ------------------------------------------------------------
-  public void    remove  (short e) { 
+  public void remove(short e) {
     // TODO - A COMPLETER...
   }
+
   // ------------------------------------------------------------
   public boolean containsKey(short k) {
-    return false; // TODO - A COMPLETER...
+    for (short i = 0; i < size; i++){
+      if (id[i] == k)
+        return true;
+    }
+    return false;
   }
+
   // ------------------------------------------------------------
-  public boolean isEmpty() { return size() == 0; }
-  public int     size()    { return size;        }
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
   // ------------------------------------------------------------
-  //a.union(b) :        a becomes "a union b"
-  //  images are those in b whenever possible
+  // a.union(b) : a becomes "a union b"
+  // images are those in b whenever possible
   public void union(ShortToStringMap m) {
     // TODO - A COMPLETER...
   }
+
   // ------------------------------------------------------------
-  //a.intersection(b) : "a becomes a intersection b"
-  //  images are those in b
+  // a.intersection(b) : "a becomes a intersection b"
+  // images are those in b
   public void intersection(ShortToStringMap s) {
     // TODO - A COMPLETER...
   }
+
   // ------------------------------------------------------------
-  //a.toString() returns all elements in 
+  // a.toString() returns all elements in
   // a string like: {3:"abc",9:"xy",-5:"jk"}
-  public String toString(){
-    return null; 
+  public String toString() {
+    return null;
   }
 }
